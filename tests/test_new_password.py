e705b0ce76ceb9e241004a8c9fb84cf45ec0aeb3
who? <164739638+Floppat@users.noreply.github.com>
2025-06-29 12:39:48 +0300

    Update test_new_password.py

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -8,10 +8,22 @@ def test_password_characters():
     for char in password:
         assert char in valid_characters
 
+def test_password_length():
+    """Тест, что при генерации используются только допустимые символы"""
+    length = 100
+    password = generate_password(length)  # Генерируем длинный пароль для более надежной проверки
+    assert len(password) == length
+
+def test_password_dif():
+    """Тест, что при генерации используются только допустимые символы"""
+    length = 100
+    password1 = generate_password(length)  # Генерируем длинный пароль для более надежной проверки
+    password2 = generate_password(length)
+    assert password1 != password2
 """
 Допиши еще один тест из предложенных. Или придумай свой.
 Если сможешь написать больше, то будет круто!
 
-Тест, что длина пароля соответствует заданной
+Тест, что длина пароля соответствует заданнойa
 Тест, что два сгенерированных подряд пароля различаются
 """
