c12ece498306e2aab807618121305f33c2f271e5
mukhlisa <khmukhliskaa12@gmail.com>
2025-07-06 15:28:49 +0500

    mukh up

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -1,29 +1,28 @@
 import string
 from password.new_password import generate_password
+import pytest  # нужен для теста с исключением
 
-def test_password_characters():
-    """Тест, что при генерации используются только допустимые символы"""
-    valid_characters = string.ascii_letters + string.digits + string.punctuation
-    password = generate_password(100)  # Генерируем длинный пароль для более надежной проверки
+def test_password_has_valid_characters():
+    """Пароль содержит только допустимые символы"""
+    password = generate_password(20)
+    allowed = string.ascii_letters + string.digits + string.punctuation
     for char in password:
-        assert char in valid_characters
+        assert char in allowed
 
-def test_password_length():
-    """Тест, что при генерации используются только допустимые символы"""
-    length = 100
-    password = generate_password(length)  # Генерируем длинный пароль для более надежной проверки
-    assert len(password) == length
+def test_password_correct_length():
+    """Пароль имеет правильную длину"""
+    password = generate_password(20)
+    assert len(password) == 20
 
-def test_password_dif():
-    """Тест, что при генерации используются только допустимые символы"""
-    length = 100
-    password1 = generate_password(length)  # Генерируем длинный пароль для более надежной проверки
-    password2 = generate_password(length)
+def test_passwords_are_different():
+    """Два пароля не совпадают"""
+    password1 = generate_password(20)
+    password2 = generate_password(20)
     assert password1 != password2
-"""
-Допиши еще один тест из предложенных. Или придумай свой.
-Если сможешь написать больше, то будет круто!
 
-Тест, что длина пароля соответствует заданнойa
-Тест, что два сгенерированных подряд пароля различаются
-"""
+def test_empty_password():
+    """Пароль длиной 0 — пустая строка"""
+    assert generate_password(0) == ""
+
+
+
